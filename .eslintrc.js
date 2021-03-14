a317d492e89b7d1a9e39e0b1556c9477b566d955
Miriam <miriam.haenle@gmail.com>
2021-03-14 09:08:34 +0100

    Added linters, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'eslint:recommended'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  globals: {
+    React: 'writable',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+    ecmaVersion: 12,
+  },
+  plugins: ['react'],
+}
