64bcebb27403334ae83cb1b7492a5f94dd540b5c
Marie-Carleen Frei <lene@7freiheiten.de>
2021-05-02 14:38:22 +0200

    Add default head for all components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import PropTypes from 'prop-types'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
