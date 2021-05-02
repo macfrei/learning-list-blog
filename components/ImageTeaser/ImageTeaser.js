5fec92816b7b1850d2899de30a3c3fca74779540
Miriam <miriam.haenle@gmail.com>
2021-05-02 16:31:15 +0200

    Updated ImageTeaser component and added flip functionality

--- components/ImageTeaser/ImageTeaser.js
+++ components/ImageTeaser/ImageTeaser.js
@@ -1,9 +1,38 @@
 import styled from 'styled-components/macro'
 import PropTypes from 'prop-types'
 
-export default function ImageTeaser({ imgUrl, onShowDetails, altText }) {
-  return <Image src={imgUrl} onClick={onShowDetails} alt={altText} />
+export default function ImageTeaser({
+  imgUrl,
+  onShowDetails,
+  altText,
+  author,
+  flip,
+}) {
+  return (
+    <>
+      {flip ? (
+        <Details>
+          <h2>{author.name}</h2>
+          <a href={author.gitHub}>Find me on GitHub</a>
+        </Details>
+      ) : (
+        <Image src={imgUrl} onClick={onShowDetails} alt={altText} />
+      )}
+    </>
+  )
 }
+
+const Details = styled.div`
+  background-image: linear-gradient(to right, #fa709a 0%, #fee140 100%);
+  border-radius: 50%;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 15rem;
+  height: 15rem;
+`
 
 const Image = styled.img`
   border-radius: 50%;
@@ -15,4 +44,6 @@ ImageTeaser.propTypes = {
   imgUrl: PropTypes.string.isRequired,
   onShowDetails: PropTypes.func.isRequired,
   altText: PropTypes.string.isRequired,
+  author: PropTypes.object.isRequired,
+  flip: PropTypes.bool,
 }
